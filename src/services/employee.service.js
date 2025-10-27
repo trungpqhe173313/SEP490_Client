f050989cea8b499dc73303a678cb8c9391d0185a
duclahe182040 <duclahe182040@fpt.edu.vn>
2025-10-27 08:43:28 +0700

    SG-51 Feat: Employee CRUD UI

--- src/services/employee.service.js
+++ src/services/employee.service.js
@@ -1,24 +1,24 @@
 import API from '@/utils/axios';
 
 export const employeeService = {
-    getAllEmployees: async () => {
-        const response = await API.get('/employees');
+    getAllEmployees: async (data) => {
+        const response = await API.post('/employees/GetData', data);
         return response.data;
     },
     getEmployeeByID: async (id) => {
-        const response = await API.get(`/employees/${id}`);
+        const response = await API.get(`/employees/GetByUserId/${id}`);
         return response.data;
     },
     createEmployee: async (data) => {
-        const response = await API.post('/employees', data);
+        const response = await API.post('/employees/CreateEmployee', data);
         return response.data;
     },
     updateEmployee: async (id, data) => {
-        const response = await API.put(`/employees/${id}`, data);
+        const response = await API.put(`/employees/UpdateEmployee/${id}`, data);
         return response.data;
     },
     deleteEmployee: async (id) => {
-        const response = await API.delete(`/employees/${id}`);
+        const response = await API.delete(`/employees/DeleteEmployee/${id}`);
         return response.data;
     }
 };
